bc09aa8e8489966596b1e10bbaf247858a0d6ede
AdamNirvana <147259296+adamnirvan@users.noreply.github.com>
2026-05-19 22:21:02 +0700

    Fix HTTPS Mixed Content & push compiled assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Tambahkan ini di atas
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Paksa Laravel pakai HTTPS di Vercel agar CSS tidak diblokir
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
